773fc294ab48ae4ffbd2494890dad0a6cc1f86a1
Wasilewski6860 <91448408+Wasilewski6860@users.noreply.github.com>
2021-12-17 17:47:35 +0300

    Add files via upload

--- Task10/src/ru/vsu/baryshev/logic.java
+++ Task10/src/ru/vsu/baryshev/logic.java
@@ -23,13 +23,29 @@ public class logic {
         }
         return pc;
     }
+    public static PCTablet newProcessing(PCTablet pc1, PCTablet pc2){
+        if (pc1.storage != pc2.storage)
+        {
+            return pc1.storage > pc2.storage ? pc1 : pc2;
+        } else
+        if (pc1.rating != pc2.rating)
+        {
+            return pc1.rating > pc2.rating ? pc1 : pc2;
+        }else if(pc1.nameOfModel.equals("Asus") || pc1.nameOfModel.equals("Samsung")){
+            return  pc1;
+        }else if ((pc2.nameOfModel.equals("Asus") || pc2.nameOfModel.equals("Samsung"))){
+            return pc2;
+        }else return null;
+    }
 
     public static PCTablet processing(List<PCTablet> list, int price) {  // Основной метод, возвращающий конечный результат
         PCTablet pc1 = list.get(0);
 
         for (int i=0; i< list.size()-1;i++){
-           PCTablet helpPCT = processingForTwoPCT(pc1,list.get(i+1),price);
-           if (helpPCT!=null) pc1 = helpPCT;
+           PCTablet helpPCT = newProcessing(pc1,list.get(i+1));
+           if (helpPCT.price<=price){ pc1 = helpPCT;} else pc1 = null;
+
+
         }
         return pc1;
     }
